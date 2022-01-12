396302129c95ee27001a887c112f52eeb58e4cf3
srokks <srokks@gmail.com>
2022-01-12 10:38:50 +0100

    get lang index from game

--- main.lua
+++ main.lua
@@ -9,14 +9,12 @@ print('LangAPI loaded')
 langAPI.lang_index = 0
 function langAPI.setLangIndex()
     local lang_codes = {en=0,jp=2,es=4,de=5,ru,kr=11,zh=13}
-    langAPI.lang_index = tonumber(langAPI.table.languages[lang_code].index)
+    langAPI.lang_index = tonumber(langAPI.table.languages[lang_codes[Options.Language]].index)
 end
 langAPI.setLangIndex()
 function langAPI.getItemName(code_name)
-    local lang_index = langAPI.getLangIndex()
     return langAPI.table.category.Items[code_name][langAPI.lang_index]
 end
 function langAPI.getPocketName(code_name)
-    local lang_index = langAPI.getLangIndex()
     return langAPI.table.category.PocketItems[code_name][langAPI.lang_index]
 end
