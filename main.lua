04c6428ba69bc5c0edf06bcc3e3f7f8fc9aee0e9
srokks <srokks@gmail.com>
2022-01-12 10:53:14 +0100

    rework

--- main.lua
+++ main.lua
@@ -5,16 +5,5 @@
 ---
 langAPI = RegisterMod("langAPI", 1)
 include('langAPI_tables.lua')
-print('LangAPI loaded')
-langAPI.lang_index = 0
-function langAPI.setLangIndex()
-    local lang_codes = {en=0,jp=2,es=4,de=5,ru,kr=11,zh=13}
-    langAPI.lang_index = tonumber(langAPI.table.languages[lang_codes[Options.Language]].index)
-end
-langAPI.setLangIndex()
-function langAPI.getItemName(code_name)
-    return langAPI.table.category.Items[code_name][langAPI.lang_index]
-end
-function langAPI.getPocketName(code_name)
-    return langAPI.table.category.PocketItems[code_name][langAPI.lang_index]
-end
+include('langAPI.lua')
+print('LangAPI successfully loaded')
