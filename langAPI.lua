08f67c7b39e690a11f3fb740fda6f2e7bf931e6e
srokks <srokks@gmail.com>
2022-06-25 19:23:49 +0200

    easy copy/paste coopHUD mod integration

--- langAPI.lua
+++ langAPI.lua
@@ -1,3 +1,4 @@
+local langAPI = include('helpers.langAPI_tables.lua')
 langAPI.lang_index = 0
 function langAPI.setLangIndex()
     local lang_codes = {en=0,jp=2,es=4,de=5,ru,kr=11,zh=13}
@@ -10,3 +11,4 @@ end
 function langAPI.getPocketName(code_name)
     return langAPI.table.category.PocketItems[code_name][langAPI.lang_index]
 end
+return langAPI
